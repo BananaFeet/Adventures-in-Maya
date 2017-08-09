c1cc4cfbec6ebb3b4e734d636648579551295189
BananaFeet <himi86@gmail.com>
2017-08-09 16:32:47 -0700

    Update Rename.py
    Not using a prompt dialogue box this time

--- Rename.py
+++ Rename.py
@@ -1,23 +1,39 @@
-'''Rename items in scene'''
-
-
 import maya.cmds as mc
 
-obj = mc.ls(sl = True)
+#Selection
+sel = mc.ls(sl = True)
+items = sel
 
-print obj
+#Makes window
+win = mc.window()
+mc.rowColumnLayout(nc = 2, cw = [(1, 100), (2, 150), (3, 100)])
+'''
+sel1 = mc.textField()
+sel2 = mc.textField()
+sel3 = mc.textField()
+sel4 = mc.textField()
+'''
+#For every selected object, show the name and create a textfield!
+for count,name in enumerate(items):
+     #print(name)
+     #print(count)
+     mc.text( label=items[count] )
+     mc.textField(tx=items[count])
+     
+'''
+mc.textField(sel1, tx=items[0], edit=True)
+mc.textField(sel2, tx=items[1], edit=True)
+mc.textField(sel3, tx=items[2], edit=True)
+mc.textField(sel4, tx=items[3], edit=True)
+'''
+#sel1text = mc.textField(sel1)
+#userTex = mc.textField(sel, query=True, text=True)
 
-# Creates a prompt dialogue box
-result = mc.promptDialog(
-		title='Rename Object ',
-		message='Enter Name:',
-		button=['OK', 'Cancel'],
-		defaultButton='OK',
-		cancelButton='Cancel',
-		dismissString='Cancel')
+mc.showWindow(win)
 
-#function
-if result == 'OK':
-	userTex = mc.promptDialog(query=True, text=True)
-	mc.rename(obj, userTex)
-#print mc.ls(sl = True)
+
+
+
+
+
+    
