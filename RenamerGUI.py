1bcfe86db790b0b2e3c3b1ec53ff715feb5165b3
BananaFeet <himi86@gmail.com>
2017-09-05 18:12:52 -0700

    Update RenamerGUI.py

--- RenamerGUI.py
+++ RenamerGUI.py
@@ -2,7 +2,7 @@ import maya.cmds as mc
 import sys
 from PySide import QtCore, QtGui, QtUiTools
 
-
+#Imports .ui file
 class RenamerUI(QtGui.QWidget):
     def __init__(self):
         QtGui.QWidget.__init__(self)
@@ -10,23 +10,33 @@ class RenamerUI(QtGui.QWidget):
         file = QtCore.QFile("C:\Users\michelle.hill\PycharmProjects\Learning\Renamer.ui")
         file.open(QtCore.QFile.ReadOnly)
         self.myWidget = loader.load(file, self)
-
-        #Add button functions
+        
+        #Add button functions below
 
 
         file.close()
 
 
 #MAYA FUNCTIONS BELOW...
+    def textEdit(self):
+        self.myLayout = QVBoxLayout(RenamerUI)
+        obj = mc.ls(sl=True)
+        items = obj[0:]
 
+#Select objects..."x" selections will generate "x" text fields in window
+#sel = mc.ls(sl=True)
 
-#Selection will add more text fields
+#Text field
+#textBox = QWidget.QLineEdit()
+#items = obj[0:]
+
+#for x in items:
 
 
 if __name__ == "__main__":
     # Set up application with any command line args
     #app = QtGui.QApplication(sys.argv)
-
+    
     ui = RenamerUI()
 
     ui.show()
